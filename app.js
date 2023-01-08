f7decdb1487be7f28d71267cfb2b2c76b1386575
Thierry AGNELLI <thierry.agnelli@gmail.com>
2023-01-09 00:11:18 +0100

    Commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const authentication = require("./middleware/authentification");
 
-
 module.exports = (io) => {
 
     // Middleware d'authentification
@@ -9,15 +8,24 @@ module.exports = (io) => {
     // event connexion
     io.on("connection", (socket) => {
         console.log("New Challenger: " + socket.username + "!");
+        
+        if(io.usersList)
+            io.usersList.push(socket.username);
+        else
+            io.usersList = [socket.username]
 
         // AJout de la socket à la room1
         socket.join("room1");
+        
+        io.emit("users", {
+            usersList: io.usersList
+        });
 
         socket.to("room1").emit("message", {
             content: "A wild " + socket.username + " has appeared.",
             username: "Server"
         });
-
+        
         // ecoute event 
         socket.on("message", (data) => {
             console.log("New message from: " + socket.username, ":", data.content);
@@ -26,5 +34,18 @@ module.exports = (io) => {
                 username: socket.username
             });
         });
+
+        socket.on("disconnect", reason => {
+            const index = io.usersList.indexOf(socket.username);
+            
+            io.usersList.splice(index,1);
+
+            io.emit("users", {
+                usersList: io.usersList
+            });
+        });
+
     });
+
+    
 };
